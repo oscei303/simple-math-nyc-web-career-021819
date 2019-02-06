258caf7d1cac1a9ad05688b571ade021687cc014
OSCAR RIOS <oscr.rss@gmail.com>
2019-02-06 17:22:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,21 @@
 def addition(num1, num2)
+  1 + 1
 end
 
 def subtraction(num1, num2)
+  1 - 1
 end
 
 def division(num1, num2)
+  1 / 1 
 end
 
 def multiplication(num1, num2)
+  1 ** 1
 end
 
 def modulo(num1, num2)
+  
 end
 
 def square_root(num)
