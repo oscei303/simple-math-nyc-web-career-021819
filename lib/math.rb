cf2a19862ca6838f26a598d429aa810b6a28a4ac
OSCAR RIOS <oscr.rss@gmail.com>
2019-02-06 17:27:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,27 +1,27 @@
 def addition(num1, num2)
-  1 + 1
+  3 + 6
 end
 
 def subtraction(num1, num2)
-  1 - 1
+  6 - 1
 end
 
 def division(num1, num2)
-  1 / 1 
+  25 / 1 
 end
 
 def multiplication(num1, num2)
-  1 ** 1
+  120 ** 1
 end
 
 def modulo(num1, num2)
-  10 % 3
+  34 % 5
 end
 
 def square_root(num)
-  Math.sqrt(25)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-   122 + (( 2 * 24) / 3.0 )
+   7 + (( 43 * 23) / 3.0 )
 end
